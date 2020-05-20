e06745ecaac609d38059cf7b3874d424ee865aa0
Matine temory <temorymatine@gmail.com>
2020-05-20 01:35:03 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -19,8 +19,8 @@ def update_status
          ]
       }
       
-      
-  epic_tragedy[:montague][:hero][:status] = "dead"
+   }
+epic_tragedy[:montague][:hero][:status] = "dead"
 
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
