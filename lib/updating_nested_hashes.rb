35dae9313916140f43cdf12268f7eb0aee52e0a9
Matine temory <temorymatine@gmail.com>
2020-05-20 01:42:45 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -51,7 +51,8 @@ def add_characters
    }
 
    epic_tragedy[:additional_characters][0] = {name: "Prince Escalus"}
-      epic_tragedy[:additional_characters][1]={name: "Apothecary"}
+   
+  epic_tragedy[:additional_characters][1]={name: "Apothecary"}
 
 
 
